3540cfc9e048e87f9b793cdc60ffab1b7dcbb6b4
LauanyBrigido13 <lauany0809@gmail.com>
2023-05-17 10:14:29 -0400

    projeto89-atualizaçao

--- PRO-C88-Ativ-Prof-1-1_m-solucao-main-master/PRO-C88-Ativ-Prof-1-1_m-solucao-main/screens/StarMap.js
+++ PRO-C88-Ativ-Prof-1-1_m-solucao-main-master/PRO-C88-Ativ-Prof-1-1_m-solucao-main/screens/StarMap.js
@@ -1,8 +1,18 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {Text, TextInput, View, StyleSheet} from "react-native";
+import {WebView} from 'react-native-webview';
 
 export default class StarMapScreen extends Component{
+    constructor(){
+        super()
+        this.state={
+            longitude: ' ',
+            latitude: ' '
+        }
+    }
     render(){
+        const{longitude, latitude} = this.state;
+        const path= `https://virtualsky.lco.global/embed/index.html?longitude=${longitude}&latitude=${latitude}&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=true&projection=stereo&showdate=false&showposition=false`
         return(
             <View style={{
                 flex: 1,
@@ -10,7 +20,36 @@ export default class StarMapScreen extends Component{
                 alignItems: "center"
             }}>
                 <Text>Mapa das estrelas</Text>
+                <WebView
+                scalesPageToFit={true}
+                source={{uri: path}}
+                style={{marginTop: 20,
+                        marginBottom: 20,}}/>
+
+                        <TextInput
+                        style={styles.inputStyle}
+                        placeholder="Digite sua latitude"
+                        placeholderTextColor={white}
+                        onChangeText={(text)=>{
+                            this.setState({
+                                latitude: text
+                            })
+                        }}/>
             </View>
         )
     }
 }
+    const styles = StyleSheet.create({
+        inputStyle:{
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 20,
+        marginTop: 20,
+        marginRight: 20,
+        marginLeft: 20,
+        textAlign: 'center',
+        color: 'white',
+        width: 200
+        }
+    })
